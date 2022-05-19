64665b4a30ba1952c2107dc99bde06973c39b819
Asik <asikmahamud91@gmail.com>
2022-05-19 09:29:33 +0600

    new projext add

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter' : ['Inter', 'sans-serif'],
+        'open-sans' :['Open Sans', 'sans-serif']
+      },
+      
+      colors :{
+       'primary' : '#1C3C6B',
+        'secondary' : '#FCCD03',
+         'background-color' : '#EBF0F4',
+         'bg' : '#4963D1'
+   }
+    },
+    container:{
+      center: true,
+      padding: '1rem',
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1170px',
+      }
+    }
+  },
+  plugins: [],
+}
